60f777e843949f8c35f4f8d37ad4e64d73e8a78c
shauryaagarwal1997 <75524612+shauryaagarwal1997@users.noreply.github.com>
2021-02-16 07:47:13 +0000

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,60 @@
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+const Constraint=Matter.Constraint;
+var treeObj, stoneObj,groundObject, launcherObject;
+var mango1,mango2,mango3,mango4,mango5,mango6,mango7,mango8;
+var world,boy;
+var stone;
+
+function preload(){
+	boy=loadImage("images/boy.png");
+	stone=loadImage("images/stone.png");
+  }
+
+function setup() {
+	createCanvas(1300, 600);
+	engine = Engine.create();
+	world = engine.world;
+
+	mango1=new mango(1100,100,30);
+	mango2=new mango(980,100,30);
+	mango3=new mango(1000,190,30);
+	mango4=new mango(1170,160,30);
+	mango5=new mango(920,180,30);
+	mango6=new mango(1100,200,30);
+	mango7=new mango(1230,230,30);
+	mango8=new mango(970,250,30);
+
+	stone=new Stone(200,200,20,20);
+
+	treeObj=new tree(1050,580);
+	groundObject=new ground(width/2,600,width,20);
+	
+	Engine.run(engine);
+
+}
+
+function draw() {
+
+  background(230);
+  //Add code for displaying text here!
+  image(boy ,200,340,200,300);
+  
+
+  treeObj.display();
+  mango1.display();
+  mango2.display();
+  mango3.display();
+  mango4.display();
+  mango5.display();
+  mango6.display();
+  mango7.display();
+  mango8.display();
+  stone.display();
+
+  groundObject.display();
+}
